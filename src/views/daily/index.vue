8e9b41e07f94c2afe3610cd4f2dc3d66e67e9287
liuyouren <liuyouren@shencom.cn>
2018-04-25 15:07:07 +0800

    update

--- src/views/daily/index.vue
+++ src/views/daily/index.vue
@@ -1,3 +1,76 @@
 <template>
-  <div>每日金币</div>
+  <div class="daily">
+    <div class="daily-top">
+      每日金币
+    </div>
+    <div class="daily-content">
+      <div class="daily-content_top">
+        <div><img src="../../assets/images/money.png" alt=""></div>
+        <div><span>5000</span></div>
+      </div>
+      <div class="daily-content_money">
+        <div><img src="../../assets/images/money.png" alt=""></div>
+        <div>每日首次登陆获得5金币</div>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped>
+.daily {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 0.4rem 0.2rem;
+  background: url('../../assets/images/bg.jpg') no-repeat center/cover;
+  &-top {
+    color: #fff;
+    font-size: 0.48rem;
+    padding-left: 0.45rem;
+  }
+  &-content {
+    position: absolute;
+    top: 2.6rem;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+    &_top {
+      display: flex;
+      justify-content: flex-start;
+      padding-left: 0.45rem;
+      img {
+        width: 0.8rem;
+        height: 0.8rem;
+        vertical-align: center;
+      }
+      span {
+        margin-left: 0.2rem;
+        font-size: 0.56rem;
+        color: #ffe02e;
+      }
+    }
+    &_money {
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 0.36rem;
+      text-align: center;
+      color: #fff;
+      font-size: 0.36rem;
+      img{
+        width: 3rem;
+        height: 3rem;
+      }
+    }
+  }
+}
+</style>
+
+
--- src/views/rule/index.vue
+++ src/views/rule/index.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>规则页面
-    
+  <div class="rule">
+    <div class="rule-top">
+      游戏规则
+    </div>
+    <div class="rule-content">
+      <p v-for="(item, index) in 50" :key="index"><span>{{index+1}}.</span>{{item}}11111111111111111111111111111111111111111111111111111111111111111111111111111111</p>
+    </div>
+    <div class="rule-bottom">
+      底部3奥术大师多多多多多多多多多
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped>
+.rule {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 0.4rem 0.2rem;
+  background: url('../../assets/images/bg.jpg') no-repeat center/cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  &-top {
+    height: 1rem;
+    color: #fff;
+    font-size: 0.48rem;
+    padding-left: 0.45rem;
+  }
+  &-content {
+    flex: 1;
+    overflow: hidden;
+    overflow-y: scroll;
+    padding: 0 0.45rem;
+    p {
+      color: #fff;
+      font-size: 14px;
+      line-height: 0.4rem;
+      word-wrap: break-word;
+      margin-top: 0.2rem;
+      span {
+        letter-spacing: 3px;
+      }
+    }
+  }
+  &-bottom {
+    height: 2.5rem;
+  }
+}
+</style>
+
+
