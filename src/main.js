5393400a2b2566ea34335c614b485dbc1b2cef37
帅领 <xushuailing.@shencom.cn>
2018-05-30 19:15:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -33,14 +33,14 @@ router.beforeEach((to, from, next) => {
 });
 
 const getWxInfo = () => {
-  const openid = utils._Storage.get('openid') || {};
+  // const openid = utils._Storage.get('openid') || {};
   function GetQueryString(name) {
     const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`);
     const r = window.location.search.substr(1).match(reg);
     if (r != null) return decodeURIComponent(r[2]);
     return null;
   }
-  if (openid.id) return;
+  // if (openid.id) return;
   if (GetQueryString('openid') == null) {
     location.href = `http://saas.zeego.cn/interface/WJApp/WXAuth.aspx?info=1&backurl=${encodeURIComponent(location.href.split('#')[0])}`;
     return;
